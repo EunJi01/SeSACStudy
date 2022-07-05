435a9bdd088f6b40bb6038d30a16e24581b036b1
EunJi01 <hej0102@naver.com>
2022-07-05 15:05:07 +0900

    Button

---
+++ 1. Movie, Food/Food/Food/FirstViewController.swift
@@ -0,0 +1,8 @@
+//
+//  FirstViewController.swift
+//  Food
+//
+//  Created by 황은지 on 2022/07/05.
+//
+
+import Foundation
---
+++ 1. Movie, Food/Food/Food/SecondViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SecondViewController.swift
+//  Food
+//
+//  Created by 황은지 on 2022/07/05.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    // 아웃렛 변수
+    @IBOutlet weak var bannerImageView: UIImageView!
+    @IBOutlet weak var nameLable: UILabel!
+    
+    // 뷰컨트롤러 생명주기 종류 중 하나
+    // 사용자에게 화면이 보이기 직전에 실행되는 코드(보이고 나서 바뀌면 웃기니까...)
+    // option command < >
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bannerImageView.image = UIImage(named: "banner3")
+        bannerImageView.layer.cornerRadius = 70
+        bannerImageView.layer.borderWidth = 5
+        bannerImageView.layer.borderColor = UIColor.blue.cgColor
+        
+        nameLable.text = "홍길동"
+        nameLable.backgroundColor = .lightGray
+        nameLable.textColor = .red
+        nameLable.font = .boldSystemFont(ofSize: 30)
+    }
+    
+    @IBAction func resultButtonClicked(_ sender: UIButton) {
+        
+        bannerImageView.image = UIImage(named: "banner\(Int.random(in: 1...3))")
+        
+    }
+    
+}
