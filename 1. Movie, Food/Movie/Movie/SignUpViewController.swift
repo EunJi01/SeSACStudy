76b7b97e2908531a58ad98f450bd8e7316c1687b
EunJi01 <hej0102@naver.com>
2022-07-06 23:27:05 +0900

    Add: SignUp View

---
+++ 1. Movie, Food/Movie/Movie/SignUpViewController.swift
@@ -0,0 +1,65 @@
+//
+//  SignUpViewController.swift
+//  Movie
+//
+//  Created by 황은지 on 2022/07/06.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var titleLable: UILabel!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var subLabel: UILabel!
+    @IBOutlet weak var additionalInformationSwitch: UISwitch!
+    
+    @IBOutlet var textFieldList: [UITextField]!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var nicknameTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var recommendedCodeTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleLable.text = "JACKFLIX"
+        titleLable.textColor = .systemPink
+        titleLable.textAlignment = .center
+        titleLable.font = .systemFont(ofSize: 10)
+        titleLable.font = .boldSystemFont(ofSize: 40)
+        
+        signUpButton.setTitle("회원가입", for: .normal)
+        signUpButton.setTitle("가입 들어갑니다 슝슝~", for: .highlighted)
+        signUpButton.tintColor = .black
+        signUpButton.backgroundColor = .white
+        signUpButton.layer.cornerRadius = 10
+        
+        emailTextField.placeholder = "이메일 주소 또는 전화번호"
+        passwordTextField.placeholder = "비밀번호"
+        nicknameTextField.placeholder = "닉네임"
+        locationTextField.placeholder = "위치"
+        recommendedCodeTextField.placeholder = "추천 코드 입력"
+        
+        emailTextField.keyboardType = .emailAddress
+        recommendedCodeTextField.keyboardType = .numberPad
+        
+        for textField in textFieldList {
+            // placeholder 색상 변경
+            textField.layer.cornerRadius = 10
+            textField.backgroundColor = .darkGray
+            textField.textAlignment = .center
+        }
+        
+        additionalInformationSwitch.onTintColor = .systemPink
+        additionalInformationSwitch.thumbTintColor = .white
+        
+        subLabel.text = "추가 정보 입력"
+    }
+    
+    @IBAction func tapGestureClicked(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+}
