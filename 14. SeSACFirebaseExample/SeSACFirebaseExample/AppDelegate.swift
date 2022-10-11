161dba5827aa4a703bf1685b415277e205a4f4e3
EunJi01 <hej0102@naver.com>
2022-10-11 18:05:01 +0900

    [Firebase] Remote Notification

---
+++ 14. SeSACFirebaseExample/SeSACFirebaseExample/AppDelegate.swift
@@ -0,0 +1,120 @@
+//
+//  AppDelegate.swift
+//  SeSACFirebaseExample
+//
+//  Created by 황은지 on 2022/10/05.
+//
+
+import UIKit
+import FirebaseCore
+import FirebaseMessaging
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
+        // 알림 시스템에 앱 등록
+        if #available(iOS 10.0, *) {
+          // For iOS 10 display notification (sent via APNS)
+          UNUserNotificationCenter.current().delegate = self
+
+          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+          UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: { _, _ in }
+          )
+        } else {
+          let settings: UIUserNotificationSettings =
+            UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+          application.registerUserNotificationSettings(settings)
+        }
+
+        application.registerForRemoteNotifications()
+        
+        // 메세지 대리자 설정
+        Messaging.messaging().delegate = self
+        
+        // 현재 등록된 토큰 가져오기
+        Messaging.messaging().token { token, error in
+          if let error = error {
+            print("Error fetching FCM registration token: \(error)")
+          } else if let token = token {
+            print("FCM registration token: \(token)")
+          }
+        }
+
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    func application(application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+      Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    // 포그라운드 알림 수신: 로컬/푸시 동일
+    // 카카오톡: 잭님과 채팅방, 푸시마다 설정, 화면마다 설정 등
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        // .banner, .list: iOS 14+
+        completionHandler([.badge, .sound, .banner, .list])
+    }
+    
+    // 푸시 클릭: 호두과자 장바구니 담는 화면
+    // 유저가 푸시를 클릭했을 때에만 수신 확인 가능
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        print("사용자가 푸시를 클릭했습니다")
+        print(response.notification.request.content.body)
+        print(response.notification.request.content.userInfo)
+        
+        let userInfo = response.notification.request.content.userInfo
+        
+        if userInfo[AnyHashable("sesac")] as? String == "project" {
+            print("SESAC PROJECT")
+        } else {
+            print("NOTHING")
+        }
+    }
+}
+
+extension AppDelegate: MessagingDelegate {
+    
+    // 토큰 갱신 모니터링: 토큰 정보가 언제 바뀔까?
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+      print("Firebase registration token: \(String(describing: fcmToken))")
+
+      let dataDict: [String: String] = ["token": fcmToken ?? ""]
+      NotificationCenter.default.post(
+        name: Notification.Name("FCMToken"),
+        object: nil,
+        userInfo: dataDict
+      )
+      // TODO: If necessary send token to application server.
+      // Note: This callback is fired at each app startup and whenever a new token is generated.
+    }
+}
---
+++ 14. SeSACFirebaseExample/SeSACFirebaseExample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SeSACFirebaseExample
+//
+//  Created by 황은지 on 2022/10/05.
+//
+
+import UIKit
+import FirebaseAnalytics
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        Analytics.logEvent("testEvent", parameters: [
+          "name": "고래밥",
+          "full_text": "안녕하세요",
+        ])
+        
+        Analytics.setDefaultEventParameters([
+          "level_name": "Caverns01",
+          "level_difficulty": 4
+        ])
+    }
+}
