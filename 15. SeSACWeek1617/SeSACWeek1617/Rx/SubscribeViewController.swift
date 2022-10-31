8af7209afb1e4b1b972e5dc5cc3538d6b901a5df
eunji <hej0102@naver.com>
2022-10-31 19:32:23 +0900

    [1031 Study] RxAlamofire, RxDataSources

--- 15. SeSACWeek1617/SeSACWeek1617/Rx/SubscribeViewController.swift
+++ 15. SeSACWeek1617/SeSACWeek1617/Rx/SubscribeViewController.swift
@@ -8,16 +8,65 @@
 import UIKit
 import RxCocoa
 import RxSwift
+import RxAlamofire
+import RxDataSources
 
 class SubscribeViewController: UIViewController {
     
     @IBOutlet weak var button: UIButton!
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
     let disposeBag = DisposeBag()
+    
+    lazy var dataSource = RxTableViewSectionedReloadDataSource<SectionModel<String, Int>> { dataSource, tableView, indexPath, item in
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        cell.textLabel?.text = "\(item)"
+        return cell
+    }
+    
+    func testRxDataSource() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        dataSource.titleForHeaderInSection = { dataSource, index in
+            return dataSource.sectionModels[index].model
+        }
+    
+        Observable.just([
+            SectionModel(model: "title", items: [1, 2, 3]),
+            SectionModel(model: "title", items: [4, 5, 6]),
+            SectionModel(model: "title", items: [7, 8, 9])
+        ])
+            .bind(to: tableView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
+    }
+    
+    func testRxAlamofire() {
+        // Success Error => <Single>
+        let url = APIKey.searchURL + "apple"
+        request(.get, url, headers: ["Authorization": APIKey.authorization])
+            .data()
+            .decode(type: SearchPhoto.self, decoder: JSONDecoder())
+            .subscribe(onNext: { value in
+                print(value.results[0].likes)
+            })
+            .disposed(by: disposeBag)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        testRxAlamofire()
+        testRxDataSource()
+        
+        Observable.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+            .skip(3) // 3개를 무시
+            .filter { $0 % 2 == 0 } // 짝수만
+            .map { $0 * 2 } // 곱하기 2
+            .subscribe { value in
+                //print("===\(value)")
+            }
+            .disposed(by: disposeBag)
         
         bindData()
     }
