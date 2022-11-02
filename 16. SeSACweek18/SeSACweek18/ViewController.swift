ee26b48274dd1052e5fc4e8d3372e23736c36eb2
eunji <hej0102@naver.com>
2022-11-02 17:12:36 +0900

    [1102 Study] API 통신 / NetworkView (기능구현X)

---
+++ 16. SeSACweek18/SeSACweek18/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SeSACweek18
+//
+//  Created by 황은지 on 2022/11/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
